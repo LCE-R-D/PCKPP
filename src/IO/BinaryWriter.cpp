b4a69035f3e3f6fcc7a1c0e86185ddd91baa3c96
MattN-L <MattNL.XBLD@gmail.com>
2025-07-06 20:24:53 -0400

    Added BinaryWriter

---
+++ src/IO/BinaryWriter.cpp
@@ -0,0 +1,72 @@
+#include "BinaryWriter.h"
+
+BinaryWriter::BinaryWriter(const std::string& filepath)
+	: mStream(filepath, std::ios::binary)
+{
+	if (!mStream) {
+		throw std::runtime_error("Failed to open file for writing: " + filepath);
+	}
+}
+
+const void BinaryWriter::SetEndianness(IO::Endianness endianness)
+{
+	mEndianness = endianness;
+}
+
+void BinaryWriter::WriteInt8(uint8_t value)
+{
+	WriteData(&value, sizeof(value));
+}
+
+void BinaryWriter::WriteInt16(uint16_t value)
+{
+	if (mEndianness == IO::Endianness::BIG)
+		value = SwapInt16(value);
+	WriteData(&value, sizeof(value));
+}
+
+void BinaryWriter::WriteInt32(uint32_t value)
+{
+	if (mEndianness == IO::Endianness::BIG)
+		value = SwapInt32(value);
+	WriteData(&value, sizeof(value));
+}
+
+void BinaryWriter::WriteWideString(const std::u16string& utf16str)
+{
+	for (char16_t ch : utf16str)
+	{
+		uint8_t bytes[2];
+		if (mEndianness == IO::Endianness::BIG)
+		{
+			bytes[0] = static_cast<uint8_t>((ch >> 8) & 0xFF);
+			bytes[1] = static_cast<uint8_t>(ch & 0xFF);
+		}
+		else
+		{
+			bytes[0] = static_cast<uint8_t>(ch & 0xFF);
+			bytes[1] = static_cast<uint8_t>((ch >> 8) & 0xFF);
+		}
+		WriteData(bytes, 2);
+	}
+}
+
+const void BinaryWriter::WriteData(const void* buffer, size_t size)
+{
+	if (!mStream.write(reinterpret_cast<const char*>(buffer), size)) {
+		throw std::runtime_error("Failed to write to file.");
+	}
+}
+
+uint16_t BinaryWriter::SwapInt16(uint16_t val)
+{
+	return (val >> 8) | (val << 8);
+}
+
+uint32_t BinaryWriter::SwapInt32(uint32_t val)
+{
+	return ((val & 0xFF) << 24) |
+		((val & 0xFF00) << 8) |
+		((val & 0xFF0000) >> 8) |
+		((val & 0xFF000000) >> 24);
+}
---
+++ src/IO/BinaryWriter.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <fstream>
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <locale>
+#include <codecvt>
+#include "IO.h"
+
+// Barebones binary writer because it's nice I guess; inspired by miku666/NessieHax/nullptr's EndiannessAwareBinaryWriter from the OMI/PCK Studio code <3
+class BinaryWriter
+{
+public:
+	BinaryWriter(const std::string& filepath);
+	~BinaryWriter()
+	{
+		if (mStream)
+			mStream.close();
+	}
+
+	const void SetEndianness(IO::Endianness endianness);
+
+	void WriteInt8(const uint8_t value);
+	void WriteInt16(const uint16_t value);
+	void WriteInt32(const uint32_t value);
+
+	void WriteWideString(const std::u16string& utf16str);
+
+	static uint16_t SwapInt16(const uint16_t value);
+	static uint32_t SwapInt32(const uint32_t value);
+
+	const void WriteData(const void* buffer, size_t size);
+
+private:
+	std::ofstream mStream;
+	IO::Endianness mEndianness = IO::Endianness::LITTLE; // default to little since Little is used by more editions of the game
+};
