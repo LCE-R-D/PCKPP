791e44c3d20e9bce74c6d8632ffbc0ae32ee1aff
MattN-L <MattNL.XBLD@gmail.com>
2025-08-12 20:15:27 -0400

    Added edit panel
    this is a mousekatool for us to use later

--- src/UI/Preview/PreviewSkin.cpp
+++ src/UI/Preview/PreviewSkin.cpp
@@ -30,9 +30,9 @@ void PreviewSkin(PCKAssetFile& file, bool reset)
         GraphicsOpenGL* graphics = gApp->GetGraphics();
         graphics->DeleteTexture(gSkinTexture);
         gSkinTexture = graphics->LoadTextureFromMemory(file.getData().data(), file.getFileSize());
-        if(&gSkinPreviewTex)
+        if (&gSkinPreviewTex)
             glGenTextures(1, &gSkinPreviewTex.id);
-        if(&gSkinPreviewFBO)
+        if (&gSkinPreviewFBO)
             glGenFramebuffers(1, &gSkinPreviewFBO.id);
     }
 
@@ -46,13 +46,13 @@ void PreviewSkin(PCKAssetFile& file, bool reset)
         gRotationX += io.MouseDelta.y * 0.25f; // pitch
     }
 
-    float width = ImGui::GetContentRegionAvail().x * 0.75f;
-    float height = ImGui::GetContentRegionAvail().y;
+    float previewWidth = ImGui::GetContentRegionAvail().x * 0.75f;
+    float previewHeight = ImGui::GetContentRegionAvail().y;
 
     glBindFramebuffer(GL_FRAMEBUFFER, gSkinPreviewFBO.id);
 
     glBindTexture(GL_TEXTURE_2D, gSkinPreviewTex.id);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, previewWidth, previewHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gSkinPreviewTex.id, 0);
@@ -62,14 +62,14 @@ void PreviewSkin(PCKAssetFile& file, bool reset)
 
     // Draw scene
     glBindFramebuffer(GL_FRAMEBUFFER, gSkinPreviewFBO.id);
-    glViewport(0, 0, width, height);
+    glViewport(0, 0, previewWidth, previewHeight);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Transparency
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    glPerspective(45.0f, width / height, 0.1f, 100.0f);
+    glPerspective(45.0f, previewWidth / previewHeight, 0.1f, 100.0f);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -90,8 +90,8 @@ void PreviewSkin(PCKAssetFile& file, bool reset)
     glEnable(GL_TEXTURE_2D);
     glEnable(GL_CULL_FACE);
 
-    Box head(gSkinTexture.width, gSkinTexture.height, 0, 0, 0, 8, 8, 8, 0, 0);
-    //Box body(gSkinTexture.width, gSkinTexture.height, 0, 0, 0, 8, 12, 4, 16, 16);
+    Box head(gSkinTexture.width, gSkinTexture.height, -8, -16, -8, 8, 8, 8, 0, 0);
+    //Box body(gSkinTexture.width, gSkinTexture.height, -8, 0, -4, 8, 12, 4, 16, 16);
 
     head.Draw();
     //body.Draw();
@@ -102,10 +102,28 @@ void PreviewSkin(PCKAssetFile& file, bool reset)
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     // Display in ImGui
-    ImGui::Image((ImTextureID)(intptr_t)gSkinPreviewTex.id, ImVec2(width, height), ImVec2(0, 1), ImVec2(1, 0));
+    ImGui::Image((ImTextureID)(intptr_t)gSkinPreviewTex.id, ImVec2(previewWidth, previewHeight), ImVec2(0, 1), ImVec2(1, 0));
 
     if (ImGui::IsItemHovered() && io.MouseWheel != 0.0f)
     {
-        gZoom -= io.MouseWheel * 0.5f; // adjust speed here
+        gZoom -= io.MouseWheel * 2.0f; // adjust scroll speed here
+    }
+
+    ImGui::SetCursorPos(ImVec2(previewWidth, ImGui::GetFrameHeight()));
+    if (ImGui::BeginChild("SkinEditorPanel", { ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y }, 0, ImGuiWindowFlags_NoTitleBar))
+    {
+        ImDrawList* draw_list = ImGui::GetWindowDrawList();
+
+        float availableSpaceX = ImGui::GetContentRegionAvail().x;
+        float availableSpaceY = ImGui::GetContentRegionAvail().y;
+
+        ImVec2 windowPos = ImGui::GetWindowPos();
+        ImVec2 windowBounds = ImVec2(windowPos.x + availableSpaceX, windowPos.y + availableSpaceY);
+
+        draw_list->AddRectFilled(windowPos, windowBounds, IM_COL32(60, 60, 60, 255)); // dark gray
+
+        ImGui::Image((ImTextureID)(intptr_t)gSkinTexture.id, { availableSpaceX, availableSpaceX });
+
+        ImGui::EndChild();
     }
 }
